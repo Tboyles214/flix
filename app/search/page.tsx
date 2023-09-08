6ea1898683675d0f8492c1ecc568189fb3e87712
Tboyles214 <132937742+Tboyles214@users.noreply.github.com>
2023-09-08 15:56:48 -0700

    fixed one more time

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -13,24 +13,24 @@ async function SearchPage({ searchParams }: { searchParams: searchParams }) {
   const searchMovies = response.Search || [];
 
   const limitedSearchMovies = searchMovies.slice(0, 6);
-
   return (
-<div className="mx-20"> 
-  <main className="pt-40 mx-auto px-8 xl:px-0">
-    <section className="flex flex-col space-y-12 pb-44 items-center">
-      <h1 className="text-5xl font-bold text-center text-black">
-        Search Results for &quot;{searchParams.searchTerm}&quot;
-      </h1>
-      <div className="mx-3 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 xl:gap-x-8">
-        {limitedSearchMovies.map((movie) => (
-          <Movie key={movie.imdbID} movie={movie} />
-        ))}
-      </div>
-    </section>
-  </main>
-</div>
-  
-  ) 
+    <div className="mx-10 md:mx-20">
+      <main className="pt-40 mx-auto px-8 xl:px-0">
+        <section className="flex flex-col space-y-12 pb-44 items-center">
+          <h1 className="text-5xl font-bold text-center text-black">
+            Search Results for &quot;{searchParams.searchTerm}&quot;
+          </h1>
+          <div className="mx-3 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 xl:gap-x-8">
+            {limitedSearchMovies.map((movie) => (
+              <div key={movie.imdbID} className="flex-shrink-0">
+                <Movie movie={movie} />
+              </div>
+            ))}
+          </div>
+        </section>
+      </main>
+    </div>
+  );
 }
 
 export default SearchPage;
